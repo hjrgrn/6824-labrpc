7c599c47ca2e022e89cc9abe5cc7a11d630758ec
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-07-09 18:01:33 +0200

    Add 6824-labgob dependency
    - Add ClientEnd.Call()

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hjrgrn/6824-labrpc
 
 go 1.22.2
+
+require github.com/hjrgrn/6824-labgob v0.0.0-20260709143604-1087d319217a
--- labrpc.go
+++ labrpc.go
@@ -50,13 +50,17 @@ package labrpc
 //
 
 import (
+	"bytes"
+	"log"
 	"reflect"
 	"sync"
+
+	"github.com/hjrgrn/6824-labgob"
 )
 
 type reqMsg struct {
-	endname  interface{} // name of sending ClientEnd
-	svcMeth  string      // e.g. "Raft.AppendEntries"
+	endname  any    // Name of sending ClientEnd
+	svcMeth  string // e.g. "Raft.AppendEntries".
 	argsType reflect.Type
 	args     []byte
 	replyCh  chan replyMsg
@@ -68,20 +72,64 @@ type replyMsg struct {
 }
 
 type ClientEnd struct {
-	endname interface{}   // this end-point's name
+	endname any           // This end-point's name
 	ch      chan reqMsg   // copy of Network.endCh
-	done    chan struct{} // closed when Network is cleaned up
+	done    chan struct{} // closed when Network is cleaned up.
+}
+
+// Send an RPC, wait for the reply.
+// the return value indicates success, false means that
+// no reply was received from the server.
+func (e *ClientEnd) Call(svcMeth string, args any, reply any) bool {
+	req := reqMsg{}
+	req.endname = e.endname
+	req.svcMeth = svcMeth
+	req.argsType = reflect.TypeOf(args)
+	req.replyCh = make(chan replyMsg)
+
+	qb := new(bytes.Buffer)
+	qe := labgob.NewEncoder(qb)
+	if err := qe.Encode(args); err != nil {
+		panic(err)
+	}
+	req.args = qb.Bytes()
+
+	//
+	// send the request.
+	//
+	select {
+	case e.ch <- req:
+		// the request has been sent.
+	case <-e.done:
+		// entire Network has been destroyed.
+		return false
+	}
+
+	//
+	// wait for the reply.
+	//
+	rep := <-req.replyCh
+	if rep.ok {
+		rb := bytes.NewBuffer(rep.reply)
+		rd := labgob.NewDecoder(rb)
+		if err := rd.Decode(reply); err != nil {
+			log.Fatalf("ClientEnd.Call(): decode reply: %v\n", err)
+		}
+		return true
+	} else {
+		return false
+	}
 }
 
 type Network struct {
 	mu             sync.Mutex
 	reliable       bool
-	longDelays     bool                        // pause a long time on send on disabled connection
-	longReordering bool                        // sometimes delay replies a long time
-	ends           map[interface{}]*ClientEnd  // ends, by name
-	enabled        map[interface{}]bool        // by end name
-	servers        map[interface{}]*Server     // servers, by name
-	connections    map[interface{}]interface{} // endname -> servername
+	longDelays     bool               // pause a long time on send on disabled connection
+	longReordering bool               // sometimes delay replies a long time
+	ends           map[any]*ClientEnd // ends, by name
+	enabled        map[any]bool       // by end name
+	servers        map[any]*Server    // servers, by name
+	connections    map[any]any        // endname -> servername
 	endCh          chan reqMsg
 	done           chan struct{} // closed when Network is cleaned up
 	count          int32         // total RPC count, for statistics
